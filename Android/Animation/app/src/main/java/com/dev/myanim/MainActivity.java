af40e5943dc891f49ffd071f443d2cb0ed8a3e2b
Md.Waliul_Islam_Rayhan <b190305034@cse.jnu.ac.bd>
2022-12-07 08:32:49 +0600

    2.0

---
+++ Android/Animation/app/src/main/java/com/dev/myanim/MainActivity.java
@@ -0,0 +1,38 @@
+package com.dev.myanim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView hello;
+    Button bt1, bt2, bt3, bt4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hello=findViewById(R.id.textView);
+        bt1=findViewById(R.id.button1);
+        bt2=findViewById(R.id.button2);
+        bt3=findViewById(R.id.button3);
+        bt4=findViewById(R.id.button4);
+
+        Animation move = AnimationUtils.loadAnimation(this, R.anim.move);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hello.startAnimation(move);
+            }
+        });
+
+    }
+}
