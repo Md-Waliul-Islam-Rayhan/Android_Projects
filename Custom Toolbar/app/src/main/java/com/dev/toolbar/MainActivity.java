0b6c9bff1f40d63585aa7a1bcf285f4690d7038b
Md.Waliul_Islam_Rayhan <b190305034@cse.jnu.ac.bd>
2022-12-10 18:12:26 +0600

    2.0

--- Custom Toolbar/app/src/main/java/com/dev/toolbar/MainActivity.java
+++ Custom Toolbar/app/src/main/java/com/dev/toolbar/MainActivity.java
@@ -1,11 +1,14 @@
 package com.dev.toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,5 +39,42 @@ Toolbar toolbar;
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.option,menu);
         return true;
+    }
+
+
+
+
+    // for inbuilt menubar
+/*    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option,menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
+
+
+    //another way
+/*    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.option,menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.l1){
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (item.getItemId()==R.id.l2){
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (item.getItemId()==R.id.l3){
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
